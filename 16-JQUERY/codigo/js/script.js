17e5dcafd15216abf17638ef14119787c9b2c8f8
Luiz Jr <luizjr18101995@gmail.com>
2024-03-01 13:59:31 -0300

    Finalizando o modulo de Jquery

---
+++ 16-JQUERY/codigo/js/script.js
@@ -0,0 +1,7 @@
+console.log($("#div3").prev());
+console.log($("#div3").prevAll());
+console.log($("#div5").prevUntil("#div2"));
+console.log($("#div3").next());
+console.log($("#div3").nextAll());
+console.log($("#div1").nextUntil("#div4"));
+console.log($("#div3").siblings());
