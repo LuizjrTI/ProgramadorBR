1dd475d4a7fbb3daf35386a8e351fbacb76e2afe
Luiz Jr <luizjr18101995@gmail.com>
2024-03-18 17:53:18 -0300

    Finalizando modulo de node

---
+++ 18-NODE/codigo/calc.js
@@ -0,0 +1,10 @@
+const calc = {
+  soma: (x, y) => {
+    return x + y;
+  },
+  multi: (x, y) => {
+    return x * y;
+  },
+};
+
+module.exports = calc;
---
+++ 18-NODE/codigo/script.js
@@ -0,0 +1,16 @@
+const calc = require("./calc");
+
+let args = process.argv.slice(2);
+
+let a = Number(args[1]);
+let b = Number(args[2]);
+let c = "";
+
+if (args[0] == "s") {
+  c = calc.soma(a, b);
+} else if (args[0] == "m") {
+  c = calc.multi(a, b);
+} else {
+  c = "Opção Inválida";
+}
+console.log("🚀 ~  c :", c);
